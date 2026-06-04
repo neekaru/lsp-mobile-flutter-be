11236b9e6d1d86a1505b3cea9e519bafd18b91d0
Neekaru <32153003+ninajika@users.noreply.github.com>
2026-06-04 15:20:50 +0700

    refactor: change app name and package identifier
    - Change app label from 'lsp_digital_mobile' to 'LSP Digital Mobile'
    - Change package name from 'com.example.lsp_digital_mobile' to 'id.lspdigital.mobile'
    - Update Android namespace and applicationId
    - Update iOS CFBundleDisplayName and CFBundleName
    - Use proper reverse domain notation for Indonesia

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.lsp_digital_mobile"
+    namespace = "id.lspdigital.mobile"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -32,10 +32,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.lsp_digital_mobile"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "id.lspdigital.mobile"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
